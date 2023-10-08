2304b968c2c33f4b2c7640da80e31cdd3829e6a4
julia <juliaflausino0511@gmail.com>
2023-10-08 19:57:45 -0300

    começando a fazer o botao de panico

--- guardiao_app/lib/screens/alarme/alarme.dart
+++ guardiao_app/lib/screens/alarme/alarme.dart
@@ -16,10 +16,65 @@ class _TelaAlarmeState extends State<TelaAlarme> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text("Você está em perigo?"),
-            Container(
-              decoration: BoxDecoration(
-                
+            const Text("Você está em perigo?"),
+            Center(
+              child: Container(
+                height: 400,
+                padding: const EdgeInsets.all(20.0),
+                child: Stack(
+                  children: [
+                    Positioned(
+                      bottom: 0,
+                      child: Container(
+                        width: 300,
+                        height: 300,
+                        decoration: BoxDecoration(
+                          color: const Color.fromARGB(255, 77, 70, 70),
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(300),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      bottom: 10,
+                      child: Container(
+                        width: 300,
+                        height: 300,
+                        decoration: BoxDecoration(
+                          color: Colors.grey,
+                          borderRadius: const BorderRadius.all(Radius.circular(300))
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      top: 3,
+                      left: 25,
+                      bottom: 75,
+                      child: Container(
+                        width: 250,
+                        height: 250,
+                        decoration: BoxDecoration(
+                          color: Color.fromARGB(255, 184, 40, 40),
+                          borderRadius: const BorderRadius.all(Radius.circular(300))
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      top: 2,
+                      left: 25,
+                      child: Container(
+                        width: 250,
+                        height: 250,
+                        decoration: BoxDecoration(
+                          color: Color.fromARGB(255, 227, 79, 79),
+                          borderRadius: const BorderRadius.all(Radius.circular(300))
+                        ),
+                      ),
+                    ),
+
+                  ],
+                ),
               ),
             ),
             ElevatedButton(
